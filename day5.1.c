a9bf4f6cb1d50e3c136f56a4d2c37901ad4579a9
divisharmawork-lang <divisharmawork@gmail.com>
2026-09-12 13:25:19 +0530

    Add simple and compound interest calculation program
    Implement a program to calculate simple and compound interest based on user input for principal, rate, and time.

---
+++ day5.1.c
@@ -0,0 +1,18 @@
+Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+  # include <stdio.h>
+# include <math.h>
+float main (){
+    int P,R,T,SI,CI;
+   printf("enter the value of P:");
+   scanf("%d",&P);
+   printf("enter the value of R:");
+   scanf("%d",&R);
+   printf("enter the value of T:");
+   scanf("%d",&T);
+   SI= (P*R*T)/100;
+   CI = P * pow((1 + R / 100.0), T) - P;
+   printf("simple intrest is %d ",SI);
+   printf("COMPOUND intrest is %d ",CI);
+return 0;
+}
