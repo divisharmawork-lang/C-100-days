f5a44263aeb226b42ac616e3872a9522ec5f8bd1
divisharmawork-lang <divisharmawork@gmail.com>
2026-09-14 22:49:49 +0530

    Create day8.1.c

---
+++ day8.1.c
@@ -0,0 +1,22 @@
+Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+
+  #include <stdio.h>
+
+int main(void) {
+    char character;
+
+    printf("Enter a character: ");
+    scanf(" %c", &character);
+
+    if (character >= 'A' && character <= 'Z') {
+        printf("It is an uppercase alphabet.\n");
+    } else if (character >= 'a' && character <= 'z') {
+        printf("It is a lowercase alphabet.\n");
+    } else if (character >= '0' && character <= '9') {
+        printf("It is a digit.\n");
+    } else {
+        printf("It is a special character.\n");
+    }
+
+    return 0;
+}
