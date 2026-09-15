3854da98758e6c301a4e214e91aea8071e5ce2a3
divisharmawork-lang <divisharmawork@gmail.com>
2026-09-15 20:46:37 +0530

    Create day24.1.c

---
+++ day24.1.c
@@ -0,0 +1,22 @@
+Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+
+#include <stdio.h>
+
+int main() {
+    int row, column;
+
+    for (row = 1; row <= 5; row++) {
+        for (column = 1; column <= row; column++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
