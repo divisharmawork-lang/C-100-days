ef08b9f6b101886b9b17a689c1851dc6d035c3a9
divisharmawork-lang <divisharmawork@gmail.com>
2026-09-15 20:54:20 +0530

    Create day25.2.c

---
+++ day25.2.c
@@ -0,0 +1,26 @@
+Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+
+#include <stdio.h>
+
+int main() {
+    int row, space, star;
+
+    for (row = 1; row <= 5; row++) {
+        for (space = 1; space < row; space++) {
+            printf(" ");
+        }
+
+        for (star = row; star <= 5; star++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
