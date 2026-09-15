92bdc253b9ae38f236ae7fee798dcb142a7ab3de
divisharmawork-lang <divisharmawork@gmail.com>
2026-09-15 20:01:51 +0530

    Create day19.1.c

---
+++ day19.1.c
@@ -0,0 +1,26 @@
+Write a program to find the LCM of two numbers.
+
+  #include <stdio.h>
+
+int main() {
+    int a, b, x, y, remainder, hcf, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
+    while (y != 0) {
+        remainder = x % y;
+        x = y;
+        y = remainder;
+    }
+
+    hcf = x;
+    lcm = (a * b) / hcf;
+
+    printf("LCM = %d", lcm);
+
+    return 0;
+}
